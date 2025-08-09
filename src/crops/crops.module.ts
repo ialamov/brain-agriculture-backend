49bd61758995e3c6b1680b5e2a1b7ce8a37094c6
Douglas de Oliveira Ialamov <douglas.ialamov@corebiz.ag>
2025-08-09 10:28:03 -0300

    fix: Corrections in crops module.

--- src/crops/crops.module.ts
+++ src/crops/crops.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CropsService } from './crops.service';
 import { CropsController } from './crops.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Crop } from '../entities/crop.entity';
+import { LoggerService } from '../common/services/logger.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Crop])],
   controllers: [CropsController],
-  providers: [CropsService],
+  providers: [CropsService, LoggerService],
 })
 export class CropsModule {}
